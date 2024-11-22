97888dec6e41844c9ffea280ebbdaeb84a6f22ef
YonatanGM <girmayonatan86@gmail.com>
2024-11-22 03:01:22 +0100

    Fix error in keplerian orbital element to state vector conversion

--- include/io.h
+++ include/io.h
@@ -7,13 +7,14 @@
 
 // Functions to parse CSV file
 bool readCSV(const std::string& filename, std::vector<Body*>& bodies);
-
-
 // Function to parse Scenario 2 CSV files (Orbital Elements)
 // bool parseScenario2(const std::string& filename, std::vector<Body*>& bodies);
-
 void saveState(const std::string& vs_dir, int vs_counter, const std::vector<Body*>& bodies);
 
 // Function to convert orbital elements CSV to state vector CSV
-bool convertOrbitalElementsToCSV(const std::string& inputFilename, const std::string& outputFilename, double mu);
+bool convertOrbitalElementsToCSV(const std::string& inputFilename, const std::string& outputFilename);
+
+// Function to combine two CSV files into one, removing duplicates based on the "name" column
+void combineCSVFiles(const std::string& inputFile1, const std::string& inputFile2, const std::string& outputFile);
+
 #endif // IO_H
--- src/orbital_converter.cpp
+++ src/orbital_converter.cpp
@@ -6,20 +6,17 @@ int main() {
     const std::string inputFilename = "data/scenario1_without_planets_and_moons.csv"; // Input file with orbital elements
     const std::string outputFilename = "state_vectors.csv";   // Output file for state vectors
 
-    // Constants
-    const double G = 0.0002959122082855911; // Gravitational constant in AU^3 kg^-1 day^-2
-    const double mass_sun = 1.98847e30;     // Mass of the Sun in kg
-
-    // Dynamically calculate mu
-    double mu = G * mass_sun; // AU^3/day^2
 
     std::cout << "Converting orbital elements to state vectors..." << std::endl;
 
-    if (!convertOrbitalElementsToCSV(inputFilename, outputFilename, mu)) {
+    if (!convertOrbitalElementsToCSV(inputFilename, outputFilename)) {
         std::cerr << "Conversion failed. Check the input file or parameters." << std::endl;
         return 1;
     }
 
     std::cout << "Conversion successful! Output written to: " << outputFilename << std::endl;
+
+    combineCSVFiles("data/planets_and_moons_state_vectors.csv", "state_vectors.csv", "planets_and_moons_and_asteroids_state_vectors.csv");
+
     return 0;
 }
