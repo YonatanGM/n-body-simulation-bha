4ac23e60643548e000867efc7b2edc0bd5393792
YonatanGM <girmayonatan86@gmail.com>
2024-11-22 02:59:34 +0100

    Fix error in keplerian orbital element to state vector conversion

--- src/kepler_to_cartesian.cpp
+++ src/kepler_to_cartesian.cpp
@@ -1,13 +1,30 @@
 #include "kepler_to_cartesian.h"
 #include <cmath>
-
+#include <iostream>
+using namespace std;
 // Convert orbital elements to Cartesian state vectors
-Body* convertKeplerToCartesian(const OrbitalElements& elem, double mu) {
+Body* convertKeplerToCartesian(const OrbitalElements& elem) {
+    
+    // Constants
+    const double G = 1.48812e-34; // Gravitational constant in AU^3 kg^-1 day^-2
+    const double mass_sun = 1.98847e30;     // Mass of the Sun in kg
+
+    // calculate mu (in AU and day)
+    double mu = G * mass_sun; // AU^3/day^2
     // Convert angles from degrees to radians if necessary
+    // std::cout << "cKtoC: " << "mu = " <<  mu << std::endl;
     double inclination = elem.inclination;
     double argOfPeriapsis = elem.argOfPeriapsis;
     double longOfAscNode = elem.longOfAscNode;
-    double meanAnomaly = elem.meanAnomaly;
+    double meanAnomaly = elem.meanAnomaly; // this assumes t0 = t1 do I need to fix this
+
+    // double desiredTime = 2451544.5; // Julian Date for J2000.0
+    // double deltaTime = desiredTime - elem.epoch; // Δt in days, elem.epoch is t0
+    // double meanMotion = sqrt(mu / pow(elem.semiMajorAxis, 3)); // Ensure a is in AU
+    // meanAnomaly += meanMotion * deltaTime;
+    // meanAnomaly = fmod(meanAnomaly, 2 * M_PI); // Normalize to [0, 2π]
+    // if (meanAnomaly < 0) meanAnomaly += 2 * M_PI;
+
 
     // Solve Kepler's Equation for Eccentric Anomaly (E)
     double E = meanAnomaly; // Initial guess
@@ -37,9 +54,9 @@ Body* convertKeplerToCartesian(const OrbitalElements& elem, double mu) {
     double y_orb = r * sin(trueAnomaly);
 
     // Velocity in orbital plane
-    double h = sqrt(mu * elem.semiMajorAxis * (1 - elem.eccentricity * elem.eccentricity));
-    double vx_orb = -mu / h * sin(E);
-    double vy_orb = mu / h * sqrt(1 - elem.eccentricity * elem.eccentricity) * cos(E);
+    double h = sqrt(mu * elem.semiMajorAxis) / r;
+    double vx_orb = h * (-sin(E));
+    double vy_orb = h * sqrt(1 - elem.eccentricity * elem.eccentricity) * cos(E);
 
     // Rotate to inertial frame
     double cos_Omega = cos(longOfAscNode);
@@ -52,13 +69,13 @@ Body* convertKeplerToCartesian(const OrbitalElements& elem, double mu) {
     // Rotation matrix components
     double R11 = cos_Omega * cos_omega - sin_Omega * sin_omega * cos_i;
     double R12 = -cos_Omega * sin_omega - sin_Omega * cos_omega * cos_i;
-    double R13 = sin_Omega * sin_i;
+    // double R13 = sin_Omega * sin_i;
     double R21 = sin_Omega * cos_omega + cos_Omega * sin_omega * cos_i;
     double R22 = -sin_Omega * sin_omega + cos_Omega * cos_omega * cos_i;
-    double R23 = -cos_Omega * sin_i;
+    // double R23 = -cos_Omega * sin_i;
     double R31 = sin_omega * sin_i;
     double R32 = cos_omega * sin_i;
-    double R33 = cos_i;
+    // double R33 = cos_i;
 
     // Position in inertial frame
     double x = R11 * x_orb + R12 * y_orb;
@@ -72,7 +89,7 @@ Body* convertKeplerToCartesian(const OrbitalElements& elem, double mu) {
 
     // Create and return the Body object
     Body* body = new Body;
-    body->mass = elem.mass;
+    // body->mass = elem.mass;
     body->x = x;
     body->y = y;
     body->z = z;
@@ -85,3 +102,4 @@ Body* convertKeplerToCartesian(const OrbitalElements& elem, double mu) {
 
     return body;
 }
+
