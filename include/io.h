c054635cbee6db08369e6886504376ed4efb172a
YonatanGM <girmayonatan86@gmail.com>
2024-12-02 03:16:39 +0100

    Update CSV reading and writing to handle references instead of pointers for MPI usage

--- include/io.h
+++ include/io.h
@@ -5,11 +5,11 @@
 #include <string>
 #include "body.h"
 
-// Functions to parse CSV file
-bool readCSV(const std::string& filename, std::vector<Body*>& bodies);
-// Function to parse Scenario 2 CSV files (Orbital Elements)
-// bool parseScenario2(const std::string& filename, std::vector<Body*>& bodies);
-void saveState(const std::string& vs_dir, int vs_counter, const std::vector<Body*>& bodies);
+// function to read bodies from a CSV file
+bool readCSV(const std::string& filename, std::vector<Body>& bodies);
+
+// function to save the state of bodies to a CSV file
+void saveState(const std::string& vs_dir, int vs_counter, const std::vector<Body>& bodies);
 
 // Function to convert orbital elements CSV to state vector CSV
 bool convertOrbitalElementsToCSV(const std::string& inputFilename, const std::string& outputFilename);
