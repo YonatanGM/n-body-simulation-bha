39ad2e3301c44980b792ac634e8da77a6be078d0
YonatanGM <girmayonatan86@gmail.com>
2024-12-02 03:11:57 +0100

    Move computeAcceleration function

--- include/barnes_hut.h
+++ include/barnes_hut.h
@@ -5,9 +5,12 @@
 #include "octree.h"
 
 // Build the octree from the list of bodies
-void buildOctree(const std::vector<Body*>& bodies, OctreeNode*& root);
+void buildOctree(const std::vector<Body>& bodies, OctreeNode*& root);
 
 // Compute forces on a body using the Barnes-Hut algorithm
-void computeForcesBarnesHut(Body& body, OctreeNode* node, double theta, double G, double softening);
+void computeForcesBarnesHut(Body& body, OctreeNode* node, double theta, double G, double softening, const std::vector<Body>& bodies);
 
-#endif // BARNES_HUT_H
+void computeAccelerations(std::vector<Body>& bodies, std::vector<Body>& local_bodies, double G, double theta, double softening);
+
+
+#endif 
