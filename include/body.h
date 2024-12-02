70bf1ee8db7d6e00815501d114591aef389a18e8
YonatanGM <girmayonatan86@gmail.com>
2024-12-02 03:14:18 +0100

    Addindex, remove string name so we can create MPI datatype out of it

--- include/body.h
+++ include/body.h
@@ -1,15 +1,17 @@
+
 #ifndef BODY_H
 #define BODY_H
 
 #include <string>
 
-// Structure representing a celestial body
+// structure representing a celestial body
 struct Body {
-    double mass;     // Mass of the body
-    double x, y, z;  // Position coordinates
-    double vx, vy, vz; // Velocity components
-    double ax, ay, az; // Acceleration components
-    std::string name;  // Name of the body
+    int index;             // unique index of body 
+    double mass;        // mass of the body
+    double x, y, z;     // position coordinates
+    double vx, vy, vz;  // velocity components
+    double ax, ay, az;  // acceleration components
+    // std::string name;   // name of the body
 };
 
 #endif // BODY_H
